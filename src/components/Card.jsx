183d167f4d3cc03646bc893fe6bd8a28652e18de
Himanshu Mehndiratta <himanshu@Himanshus-MacBook-Air.local>
2025-05-10 22:26:12 +0530

    Changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -27,7 +27,7 @@ export default React.memo(function Card({ index, movieData, isLiked = false }) {
 
   const addToList = async () => {
     try {
-      await axios.post("http://netflix-service.vercel.app/api/user/add", {
+      await axios.post("https://moviemaze-service.onrender.com/api/user/add", {
         email,
         data: movieData,
       });
